734bfad2953657f39117e5b8ccb453afee7feb36
Zahari Zvezdomirov <z.zvezdomirov@gmail.com>
2020-03-13 16:53:08 +0200

    Add files via upload

---
+++ javaeeclient/src/com/sapinternship/Main.java
@@ -0,0 +1,20 @@
+package com.sapinternship;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080"))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+    }
+}
